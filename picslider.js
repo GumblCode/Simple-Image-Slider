8878de07ae727f1ec7bfbdcc90fc58f486964589
Rainer <126818957+GumblCode@users.noreply.github.com>
2024-03-01 10:01:40 +0100

    first commit

---
+++ picslider.js
@@ -0,0 +1,40 @@
+document.querySelector(".grid").addEventListener("mouseover", () => {
+	lenis.start();
+});
+
+document.querySelector(".grid").addEventListener("mouseout", () => {
+	lenis.stop();
+});
+
+const lenis = new Lenis({
+	smooth: true,
+	infinite: true,
+	direction: "horizontal",
+	gestureDirection: "both",
+});
+
+function raf(time) {
+	lenis.raf(time);
+	requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
+
+// repeat first four items by cloning them and appending them to the .grid
+const repeatItems = (parentEl, total = 0) => {
+	const items = [...parentEl.children];
+	for (let i = 0; i <= total - 1; ++i) {
+		var cln = items[i].cloneNode(true);
+		parentEl.appendChild(cln);
+	}
+};
+
+repeatItems(document.querySelector(".grid"), 4);
+
+imagesLoaded(
+	document.querySelectorAll(".grid__item"),
+	{ background: true },
+	() => {
+		document.body.classList.remove("slidercontainer");
+	}
+);
